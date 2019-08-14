2c81c8f32c17056d309ed17b09a42f112c13e129
newtypeV2 <gianlibunao@gmail.com>
2019-08-14 15:19:29 -0400

    Added a key on received data so that the backend can send a set of data to make enemies move.

--- app/channels/character_game_channel.rb
+++ app/channels/character_game_channel.rb
@@ -9,8 +9,8 @@ class CharacterGameChannel < ApplicationCable::Channel
   end
 
   def receive(data)
-    playerInstance = CharacterGame.find(data["id"])
-    playerInstance.update(x_coordinate:data["x_coordinate"] ,y_coordinate:data["y_coordinate"], direction: data["direction"])
+    playerInstance = CharacterGame.find(data["player"]["id"])
+    playerInstance.update(x_coordinate:data["player"]["x_coordinate"] ,y_coordinate:data["player"]["y_coordinate"], direction: data["player"]["direction"])
       ActionCable.server.broadcast('character_game', data)
   end
 
