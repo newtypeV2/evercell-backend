9dc43f1fbc0ea793b93cda76c000c03ace99dbc6
newtypeV2 <gianlibunao@gmail.com>
2019-10-17 21:56:33 -0400

    reconfigured CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://evercell.now.sh/'
+    origins 'https://evercell.now.sh'
 
-    resource 'https://evercell.now.sh/',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
