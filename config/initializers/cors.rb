bc507d69302a7e3d4d7ef77d4888744994b254b0
newtypeV2 <gianlibunao@gmail.com>
2019-10-17 21:44:53 -0400

    added cors and security for websockets

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://evercell.now.sh/'
 
-    resource '*',
+    resource 'https://evercell.now.sh/',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
