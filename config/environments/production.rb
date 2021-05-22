e606f86dee11e4c0070e5910bcd923f016dfd37e
newtypeV2 <gianlibunao@gmail.com>
2021-05-22 13:45:43 -0400

    Updated ActionCable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   config.web_socket_server_url = "wss://evercell.herokuapp.com/cable" 
   # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
-  config.action_cable.allowed_request_origins = [ 'https://evercell.now.sh/', /https:\/\/evercell.now.sh*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://evercell.vercel.app/', /https:\/\/evercell.now.sh*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
